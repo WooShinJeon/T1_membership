11d26458576fae6a2ce3f07f5b95b1c93089fe5f
220-04\wlals <wow31844@gmail.com>
2025-08-29 13:16:03 +0900

    2025/08/29 13:15 member 간단한 dto,entity 업로드

---
+++ src/main/java/com/t1membership/member/constant/MemberRole.java
@@ -0,0 +1,5 @@
+package com.t1membership.member.constant;
+
+public enum MemberRole {
+    USER, BLACKLIST, ADMIN, T1PROGAMER
+}
---
+++ src/main/java/com/t1membership/member/service/MemberServiceImpl.java
@@ -0,0 +1,43 @@
+package com.t1membership.member.service;
+
+import com.t1membership.member.dto.deleteMember.DeleteMemberReq;
+import com.t1membership.member.dto.deleteMember.DeleteMemberRes;
+import com.t1membership.member.dto.joinMember.JoinMemberReq;
+import com.t1membership.member.dto.joinMember.JoinMemberRes;
+import com.t1membership.member.dto.modifyMember.ModifyMemberReq;
+import com.t1membership.member.dto.modifyMember.ModifyMemberRes;
+import com.t1membership.member.dto.readAllMember.ReadAllMemberRes;
+import com.t1membership.member.dto.readOneMember.ReadOneMemberReq;
+import com.t1membership.member.dto.readOneMember.ReadOneMemberRes;
+import com.t1membership.member.reporitory.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public JoinMemberRes joinMember(JoinMemberReq joinMemberReq) {
+        return null;
+    }
+    @Override
+    public ReadAllMemberRes readAllMember() {
+        return null;
+    }
+    @Override
+    public ReadOneMemberRes readOneMember(ReadOneMemberReq readOneMemberReq) {
+        return null;
+    }
+    @Override
+    public ModifyMemberRes modifyMember(ModifyMemberReq modifyMemberReq) {
+        return null;
+    }
+    @Override
+    public DeleteMemberRes deleteMember(DeleteMemberReq deleteMemberReq) {
+        return null;
+    }
+
+}
