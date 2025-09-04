dbfaca97966edff88d358e45c32cb74d62bd750c
220-04\wlals <wow31844@gmail.com>
2025-09-04 13:12:04 +0900

    order dto,entity 수정

--- src/main/java/com/t1membership/order/dto/OrderDto.java
+++ src/main/java/com/t1membership/order/dto/OrderDto.java
@@ -1,4 +1,19 @@
 package com.t1membership.order.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDto {
+    private Long orderNo; //주문번호
+    private String memberEmail; //회원 아이디
+    private String memberName; //회원 이름
+    private String itemName;//상품 이름
+    private int itemPrice; //상품 가격
+    private int itemStock; //상품 수량
+    private String itemImage; //상품 이미지
+    private int orderTotalPrice;//주문 총 가격
 }
